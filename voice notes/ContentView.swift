cb7f632b93cacc1149e5288281774cd70d8b9d8c
Shawn Carpenter <shawn14>
2026-01-24 17:02:56 -0600

    Initial Commit

---
+++ voice notes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  voice notes
+//
+//  Created by Shawn Carpenter on 1/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ voice notes/voice_notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  voice_notesApp.swift
+//  voice notes
+//
+//  Created by Shawn Carpenter on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct voice_notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
